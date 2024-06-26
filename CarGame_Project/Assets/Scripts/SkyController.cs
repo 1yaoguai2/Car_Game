00b29d0a5184e8797e8eede2dc4a508b9e9aeb27
yaoguai <18183849010@163.com>
2024-06-26 22:09:08 +0800

    新开项目，天空盒切换

---
+++ CarGame_Project/Assets/Scripts/SkyController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyController : MonoBehaviour
+{
+    public List<Material> skyBoxs = new List<Material>();
+    public float time = 30f;
+    private int i;
+    void Start()
+    {
+        StartCoroutine(SetSkyBox());
+    }
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+
+    public IEnumerator SetSkyBox()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(time);
+            RenderSettings.skybox = skyBoxs[i];
+            if(i < skyBoxs.Count - 1)
+            {
+                i++;
+            }
+            else
+            {
+                i = 0;
+            }
+            
+
+        }
+    }
+}
